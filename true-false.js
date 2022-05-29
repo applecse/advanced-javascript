fe89fa202e33b595712a69fa1348725d3252671a
unknown <kasembillah123@gmai.com>
2022-05-29 08:32:31 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// 0;
+// "";
+// undefined;
+// null;
+// Nan;
+// const name ="  0 "
+// ' '  '
+//truthy
+//falsy
+//false
+
+let name = 12;
+
+
+if(name || name == 0 ){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
